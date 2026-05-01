82f92ad7efd4580741c96719581324ab298bcbb7
Jan Frédéric Schmidt <jan.frederic.schmidt@icloud.com>
2026-05-02 00:38:15 +0200

    Finally implemented Userdefaults and statistic tracking and alla that

---
+++ WordGames/Data/StatView.swift
@@ -0,0 +1,18 @@
+//
+//  StatView.swift
+//  WordGames
+//
+//  Created by Jan Schmidt on 5/1/26.
+//
+
+import SwiftUI
+
+struct StatView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    StatView()
+}
